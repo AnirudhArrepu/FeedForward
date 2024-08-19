7c7ea847fed600dd38433b5ae42feb1709396d35
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-19 17:17:44 +0530

    Update Homepage.dart

--- fbm_app/lib/Pages/HomePages/Homepage.dart
+++ fbm_app/lib/Pages/HomePages/Homepage.dart
@@ -3,39 +3,39 @@ import 'package:fbm_app/Pages/Restaurant/Rprofile.dart';
 import 'package:fbm_app/Pages/User/FB_profile.dart';
 import 'package:fbm_app/Styles/BgColor.dart';
 import 'package:fbm_app/Styles/TextStyle.dart';
+import 'package:fbm_app/classes/data_class.dart';
 import 'package:fbm_app/classes/leaderboard_class.dart';
 import 'package:fbm_app/classes/notification_class.dart';
 import 'package:flutter/material.dart';
 import 'package:fbm_app/Button/button.dart';
 
 class Homepage extends StatefulWidget {
-  final  Map <String,dynamic > userDetails;
-   const Homepage({super.key, required this.userDetails});
+  final Map<String, dynamic> userDetails;
+  const Homepage({super.key, required this.userDetails});
 
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-
-   late int r;
-   late Map <String,dynamic> Profile;
+  late int r;
+  late Map<String, dynamic> Profile;
   static const List<String> route = ['/profile', '/rprofile'];
 
   @override
-   void initState() {
+  void initState() {
     super.initState();
     r = widget.userDetails['role'];
-    Profile= widget.userDetails;
+    Profile = widget.userDetails;
+    DataClass.addUsername(widget.userDetails['name']);
   }
-  
 
   @override
   Widget build(BuildContext context) {
-    NotificationClass(
-        'Leaderboard', '${LeaderboardClass.winnerDonation} is no.1 donor', false);
-    NotificationClass(
-        'Leaderboard', '${LeaderboardClass.winnerVolunteer} is no.1 volunteer', false);
+    NotificationClass('Leaderboard',
+        '${LeaderboardClass.winnerDonation} is no.1 donor', false);
+    NotificationClass('Leaderboard',
+        '${LeaderboardClass.winnerVolunteer} is no.1 volunteer', false);
 
     return Scaffold(
         backgroundColor: AppTheme.bgcolor(),
@@ -82,38 +82,40 @@ class _HomepageState extends State<Homepage> {
                 SizedBox(
                   height: 70,
                 ),
-               Row( 
-                 mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(width: 90,),
-                ElevatedButton(
-                      onPressed: () 
-                      {
-                        if (r==0){
-                          Navigator.push(context, MaterialPageRoute(builder: (c)=>FbProfile( proDetails:Profile)));
-                        }
-                        if (r==1) {
-                          Navigator.push(context, MaterialPageRoute(builder: (c)=>RestaurantProfile(RDetails: Profile))); 
-                        }
-
-                      },
-                     style: ElevatedButton.styleFrom(
-                      backgroundColor:  Color.fromARGB(255, 243, 4, 4),
-                      padding: EdgeInsets.symmetric(horizontal: 85,vertical: 15)
-                     ),
-                    
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    width: 90,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (r == 0) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (c) =>
+                                    FbProfile(proDetails: Profile)));
+                      }
+                      if (r == 1) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (c) =>
+                                    RestaurantProfile(RDetails: Profile)));
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 243, 4, 4),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 85, vertical: 15)),
                     child: Text(
                       "PROFILE",
-                      style: TextStyle(
-                       color: Colors.white,fontSize: 15 
-                      ),
-                      
-
-                    )
-                    ,
+                      style: TextStyle(color: Colors.white, fontSize: 15),
                     ),
-               ] ),
-                    SizedBox(height: 10,),
+                  ),
+                ]),
+                SizedBox(
+                  height: 10,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
