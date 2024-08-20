300f1e3f37234698695e8a4f9490668e772c6943
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-08-20 18:38:19 +0530

    update leaderboard

---
+++ fbm_app/lib/Pages/HomePages/leaderboard_donations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardDonations extends StatelessWidget {
+  const LeaderboardDonations({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ fbm_app/lib/Pages/HomePages/leaderboard_volunteer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardVolunteer extends StatelessWidget {
+  const LeaderboardVolunteer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
